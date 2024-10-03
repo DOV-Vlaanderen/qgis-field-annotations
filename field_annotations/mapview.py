a62daa17fed7fc7c8a0a64def1f18ffecbe20cdc
Roel Huybrechts <roel.huybrechts@geosparc.com>
2024-10-03 11:15:08 +0200

    Add annotation labels.

--- field_annotations/mapview.py
+++ field_annotations/mapview.py
@@ -1,4 +1,7 @@
-from qgis.core import QgsProject, QgsFillSymbol, QgsLineSymbol, QgsArrowSymbolLayer, QgsMarkerSymbol, Qgis
+from qgis.core import (
+    QgsProject, QgsFillSymbol, QgsLineSymbol, QgsArrowSymbolLayer, QgsMarkerSymbol, Qgis, QgsPalLayerSettings,
+    QgsTextFormat, QgsTextBufferSettings, QgsVectorLayerSimpleLabeling)
+from qgis.PyQt import QtGui
 
 from .translate import Translatable
 
@@ -14,6 +17,8 @@ class AnnotationLayerStyler:
             AnnotationLayerStyler.styleLineLayer(layer)
         elif geometryType == Qgis.GeometryType.Point:
             AnnotationLayerStyler.stylePointLayer(layer)
+
+        AnnotationLayerStyler.styleLabels(layer)
 
     @staticmethod
     def stylePolygonLayer(layer):
@@ -45,13 +50,42 @@ class AnnotationLayerStyler:
     @staticmethod
     def stylePointLayer(layer):
         props = layer.renderer().symbol().symbolLayer(0).properties()
-        print(props)
         props['color'] = '112,68,134,64'
         props['outline_color'] = '112,68,134,255'
         props['size'] = '3.8'
         props['outline_width'] = '0.6'
         layer.renderer().setSymbol(
             QgsMarkerSymbol.createSimple(props))
+
+    @staticmethod
+    def styleLabels(layer, field='annotation'):
+        label_settings = QgsPalLayerSettings()
+        label_settings.enabled = True
+        label_settings.fieldName = field
+
+        if layer.geometryType() == Qgis.GeometryType.Line:
+            label_settings.placement = Qgis.LabelPlacement.Curved
+        else:
+            label_settings.placement = Qgis.LabelPlacement.AroundPoint
+
+        label_format = QgsTextFormat()
+        label_format.setSize(10)
+        label_format.setNamedStyle('Regular')
+        label_format.setColor(QtGui.QColor(50, 20, 65, 255))
+
+        label_buffer = QgsTextBufferSettings()
+        label_buffer.setEnabled(True)
+        label_buffer.setSize(1)
+        label_buffer.setColor(QtGui.QColor(255, 255, 255, 192))
+        label_format.setBuffer(
+            label_buffer
+        )
+        label_settings.setFormat(
+            label_format
+        )
+
+        layer.setLabeling(QgsVectorLayerSimpleLabeling(label_settings))
+        layer.setLabelsEnabled(True)
 
 
 class AnnotationView(Translatable):
